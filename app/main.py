7d25284d4b340afc5f65c218f0069356225d7696
Danilo Galisteu <danilogalisteu@gmail.com>
2025-01-18 22:09:54 -0300

    refactoring handshake message encoding and decoding

--- app/main.py
+++ app/main.py
@@ -116,31 +116,43 @@ def get_peers(metainfo: dict, port: int=6881) -> list[tuple[str, int]]:
     return peers
 
 
-def get_handshake(host: str, port: int, info_hash_str: str) -> bytes:
+def encode_handshake(info_hash: bytes, peer_id: bytes) -> bytes:
     pstr = b"BitTorrent protocol"
     pstrlen = len(pstr)
     reserved = b"\x00\x00\x00\x00\x00\x00\x00\x00"
-    info_hash = hashlib.sha1(encode_bencode(info_hash_str)).digest()
-    peer_id = secrets.token_bytes(20)
+    
     message = bytearray(49 + pstrlen)
     message[0] = pstrlen
     message[1:1+pstrlen] = pstr
     message[1+pstrlen:1+pstrlen+8] = reserved
     message[1+pstrlen+8:1+pstrlen+8+20] = info_hash
     message[1+pstrlen+8+20:1+pstrlen+8+20+20] = peer_id
+    return message
+
+
+def decode_handshake(data: bytes) -> tuple[bytes, bytes]:
+    pstr = b"BitTorrent protocol"
+    r_pstrlen = data[0]
+    r_pstr = data[1:1+r_pstrlen]
+    assert pstr == r_pstr
+    # r_reserved_ = data[1+r_pstrlen:1+r_pstrlen+8]
+    r_info_hash = data[1+r_pstrlen+8:1+r_pstrlen+8+20]
+    r_peer_id = data[1+r_pstrlen+8+20:1+r_pstrlen+8+20+20]
+    return r_info_hash, r_peer_id
+
+
+def get_handshake(host: str, port: int, info_hash_str: str) -> bytes:
+    info_hash = hashlib.sha1(encode_bencode(info_hash_str)).digest()
+    peer_id = secrets.token_bytes(20)
+    message = encode_handshake(info_hash, peer_id)
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
         s.send(message)
         data = s.recv(1024)
         s.close()
-        r_pstrlen = data[0]
-        r_pstr = data[1:1+r_pstrlen]
-        assert pstr == r_pstr
-        # r_reserved_ = data[1+r_pstrlen:1+r_pstrlen+8]
-        r_info_hash = data[1+r_pstrlen+8:1+r_pstrlen+8+20]
+        r_info_hash, r_peer_id = decode_handshake(data)
         assert info_hash == r_info_hash
-        r_peer_id = data[1+r_pstrlen+8+20:1+r_pstrlen+8+20+20]
         return r_peer_id
 
 
