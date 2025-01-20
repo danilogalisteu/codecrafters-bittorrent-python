7af11e37f5d42fcfdb30113d8c1bf6526d1af25b
Danilo Galisteu <danilogalisteu@gmail.com>
2025-01-20 15:12:23 -0300

    load_metainfo returns raw values instead of dict

--- app/protocol/metainfo.py
+++ app/protocol/metainfo.py
@@ -12,21 +12,22 @@ def parse_metainfo_pieces(pieces: bytes) -> list[bytes]:
     return pieces_list
 
 
-def get_metainfo(file_name: str) -> dict:
+def load_metainfo(file_name: str, show_info=False) -> tuple[dict, str, bytes, bytes, int, int] | None:
     with open(file_name, "rb") as file:
         metainfo, _ = decode_bencode(file.read())
-        return metainfo
+        tracker = metainfo['announce']
+        info_hash = hashlib.sha1(encode_bencode(metainfo["info"])).digest()
+        pieces_hash = metainfo["info"]["pieces"]
+        file_length = metainfo["info"]["length"]
+        piece_length = metainfo["info"]["piece length"]
+    
+        if show_info:
+            print(f"Tracker URL: {tracker}")
+            print(f"Length: {file_length}")
+            print(f"Info Hash: {info_hash.hex()}")
+            print(f"Piece Length: {piece_length}")
+            print("Piece Hashes:")
+            for piece in parse_metainfo_pieces(pieces_hash):
+                print(piece.hex())
 
-
-def get_infohash(metainfo: dict) -> bytes:
-    return hashlib.sha1(encode_bencode(metainfo["info"])).digest()
-
-
-def print_info(metainfo: dict):
-    print(f"Tracker URL: {metainfo['announce']}")
-    print(f"Length: {metainfo['info']['length']}")
-    print(f"Info Hash: {get_infohash(metainfo).hex()}")
-    print(f"Piece Length: {metainfo['info']['piece length']}")
-    print("Piece Hashes:")
-    for piece in parse_metainfo_pieces(metainfo["info"]["pieces"]):
-        print(piece.hex())
+        return tracker, info_hash, pieces_hash, file_length, piece_length
