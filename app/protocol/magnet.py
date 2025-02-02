dcd004830e77ddd5737a311f653b553ca139cc95
Danilo Galisteu <danilogalisteu@gmail.com>
2025-02-02 01:04:00 -0300

    handle possible base32 encoding

--- app/protocol/magnet.py
+++ app/protocol/magnet.py
@@ -1,7 +1,8 @@
+from base64 import b32decode
 from urllib.parse import parse_qs, urlparse
 
 
-def parse_magnet(url: str) -> tuple[str, str]:
+def parse_magnet(url: str) -> tuple[str, list[str], str]:
     result = urlparse(url)
     assert result.scheme == "magnet"
 
@@ -11,7 +12,11 @@ def parse_magnet(url: str) -> tuple[str, str]:
     trackers = query.get("tr", [])
     info_hash = query["xt"][0]
 
-    assert len(info_hash) == 49
     assert info_hash[:9] == "urn:btih:"
+    info_hash = info_hash[9:]
 
-    return name, trackers, info_hash[9:]
+    if len(info_hash) == 32:
+        info_hash = b32decode(info_hash, casefold=True).hex()
+    assert len(info_hash) == 40
+
+    return name, trackers, info_hash
