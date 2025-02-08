71c96b85f67d0004ca4c3f59bbad5dff6655dfd6
Danilo Galisteu <danilogalisteu@gmail.com>
2025-02-07 23:18:07 -0300

    returns file name

--- app/protocol/metainfo.py
+++ app/protocol/metainfo.py
@@ -4,8 +4,8 @@ import pathlib
 from .bencode import decode_bencode, encode_bencode
 
 
-def load_metainfo(file_name: str, show_info: bool=False) -> tuple[str, bytes, bytes, int, int] | None:
-    with pathlib.Path(file_name).open("rb") as file:
+def load_metainfo(torrent_name: str, show_info: bool=False) -> tuple[str, bytes, bytes, str, int, int] | None:
+    with pathlib.Path(torrent_name).open("rb") as file:
         metainfo, _ = decode_bencode(file.read())
         assert isinstance(metainfo, dict)
         tracker = metainfo["announce"]
@@ -13,9 +13,11 @@ def load_metainfo(file_name: str, show_info: bool=False) -> tuple[str, bytes, by
         pieces_hash = metainfo["info"]["pieces"]
         file_length = metainfo["info"]["length"]
         piece_length = metainfo["info"]["piece length"]
+        file_name = metainfo["info"].get("name", "")
 
         if show_info:
             print(f"Tracker URL: {tracker}")
+            print(f"File name: {file_name}")
             print(f"Length: {file_length}")
             print(f"Info Hash: {info_hash.hex()}")
             print(f"Piece Length: {piece_length}")
@@ -23,4 +25,4 @@ def load_metainfo(file_name: str, show_info: bool=False) -> tuple[str, bytes, by
             for piece_index in range(len(pieces_hash) // 20):
                 print(pieces_hash[piece_index*20:piece_index*20+20].hex())
 
-        return tracker, info_hash, pieces_hash, file_length, piece_length
+        return tracker, info_hash, pieces_hash, file_name, file_length, piece_length
