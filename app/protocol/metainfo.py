e42c977d9a7b76092716fc16f5e4abcbd8a76504
Danilo Galisteu <danilogalisteu@gmail.com>
2025-02-14 10:46:03 -0300

    refactored TorrentInfo

--- app/protocol/metainfo.py
+++ app/protocol/metainfo.py
@@ -3,7 +3,7 @@ import pathlib
 from base64 import b32decode
 from dataclasses import dataclass, field
 from datetime import UTC, datetime
-from typing import Self
+from typing import Any, Self
 from urllib.parse import parse_qs, urlparse
 
 from .bencode import decode_bencode, encode_bencode
@@ -27,8 +27,12 @@ class TorrentInfo:
 
     info_hash: bytes = b""
     name: str = ""
+    total_length: int = 0
+    num_pieces: int = 0
     piece_length: int = 0
+    last_piece_length: int = 0
     pieces_hash: bytes = b""
+    num_files: int = 0
     files: list[FileInfo] = field(default_factory=list)
 
     private: bool = False
@@ -37,51 +41,62 @@ class TorrentInfo:
     created_by: str = ""
     creation_date: datetime = datetime.min
 
+    @staticmethod
+    def parse_files(meta_info: dict[str | bytes, Any]) -> tuple[list[FileInfo], int]:
+        display_name = meta_info.get("name", "")
+
+        if "length" in meta_info:
+            return [FileInfo(path=display_name, length=meta_info["length"], offset=0)], meta_info["length"]
+
+        files = []
+        file_offset = 0
+        for file in meta_info["files"]:
+            file_length = file["length"]
+            file_path = "/".join(file["path"])
+            if display_name:
+                file_path = display_name + "/" + file_path
+            file_info = FileInfo(
+                path=file_path,
+                length=file_length,
+                offset=file_offset,
+                mtime=datetime.fromtimestamp(float(file["mtime"]), UTC) if "mtime" in file else datetime.min,
+                md5=file.get("md5", ""),
+                sha1=file.get("sha1", ""),
+                crc32=file.get("crc32", ""),
+            )
+            files.append(file_info)
+            file_offset += file_length
+
+        return files, file_offset
+
     @classmethod
     def from_file(cls, file_name: str) -> Self | None:
         with pathlib.Path(file_name).open("rb") as metadata:
             metainfo, _ = decode_bencode(metadata.read())
             assert isinstance(metainfo, dict)
 
-            display_name = metainfo["info"].get("name", "")
-
-            if "length" in metainfo["info"]:
-                # single
-                files = [FileInfo(path=display_name, length=metainfo["info"]["length"], offset=0)]
-            else:
-                # multi
-                files = []
-                file_offset = 0
-                for file in metainfo["info"]["files"]:
-                    file_length = file["length"]
-                    file_path = "/".join(file["path"])
-                    if display_name:
-                        file_path = display_name + "/" + file_path
-                    torrent_file = FileInfo(
-                        path=file_path,
-                        length=file_length,
-                        offset=file_offset,
-                        mtime=datetime.fromtimestamp(int(file.get("mtime")), UTC) if "mtime" in file else datetime.min,
-                        md5=file.get("md5", ""),
-                        sha1=file.get("sha1", ""),
-                        crc32=file.get("crc32", ""),
-                    )
-                    files.append(torrent_file)
-                    file_offset += file_length
-
+            files, total_length = cls.parse_files(metainfo["info"])
+            num_pieces = len(metainfo["info"]["pieces"] // 20)
+            last_piece_length = total_length - num_pieces * metainfo["info"]["piece length"]
             return cls(
                 tracker=metainfo.get("announce", ""),
                 tracker_list=metainfo.get("announce-list", []),
                 info_hash=hashlib.sha1(encode_bencode(metainfo["info"])).digest(),
-                name=display_name,
+                name=metainfo["info"].get("name", ""),
+                total_length=total_length,
                 piece_length=metainfo["info"]["piece length"],
+                last_piece_length=last_piece_length,
+                num_pieces=num_pieces,
                 pieces_hash=metainfo["info"]["pieces"],
+                num_files=len(files),
                 files=files,
                 private=metainfo["info"].get("private", "0") == "1",
                 encoding=metainfo.get("encoding", ""),
                 comment=metainfo.get("comment", ""),
                 created_by=metainfo.get("created by", ""),
-                creation_date=datetime.fromtimestamp(int(metainfo.get("creation date", "0")), UTC),
+                creation_date=datetime.fromtimestamp(float(metainfo["creation date"]), UTC)
+                if "creation date" in metainfo
+                else datetime.min,
             )
 
     @staticmethod
@@ -115,7 +130,7 @@ class TorrentInfo:
 
     def show_info(self) -> None:
         print(f"Tracker URL: {self.tracker}")
-        print(f"File name: {self.name}")
+        print(f"Display name: {self.name}")
         print(f"Length: {self.files[0].length}")
         print(f"Info Hash: {self.info_hash.hex()}")
         print(f"Piece Length: {self.piece_length}")
